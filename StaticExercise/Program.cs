ceaf994e79bfcd33f9ef55eb97843e78f896dc93
Max Schlapfer <max.schlapfer@gmail.com>
2022-10-04 11:00:15 -0500

    first commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"60 Fahrenheit is: {TempConverter.FahrenheitToCelsius(60)} Celsius");
+            Console.WriteLine($"85 Fahrenheit is: {TempConverter.FahrenheitToCelsius(85)} Celsius");
+
+            Console.WriteLine("================================================");
+
+            Console.WriteLine($"18 Celsius is: {TempConverter.CelsiusToFahrenheit(18)} Fahrenheit");
+            Console.WriteLine($"15 Celsius is: {TempConverter.CelsiusToFahrenheit(15)} Fahrenheit");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        // 9/5 x + 32
+        public static double CelsiusToFahrenheit(int f)
+        {
+            return ((f * 9 / 5) + 32);
+        }
+
+        public static double FahrenheitToCelsius(int c)
+        {
+            return ((c - 32) * 5 / 9);
+        }
+    }
+}
